82483d076cc34d5051135b66423452f1c9142c53
Admin123 <fllouhalina@gmail.com>
2026-09-12 20:10:59 +0300

    нгнгнгнгнгн

---
+++ MyGameSite/Pages/Index2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SkyNet_RP.Pages
+{
+    public class Index2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
