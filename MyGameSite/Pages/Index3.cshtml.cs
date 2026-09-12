c19533154092b80df5dac298da7af6306dfe52dd
Admin123 <fllouhalina@gmail.com>
2026-09-13 00:06:23 +0300

    прпрпрпрпр

---
+++ MyGameSite/Pages/Index3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SkyNet_RP.Pages
+{
+    public class Index3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
